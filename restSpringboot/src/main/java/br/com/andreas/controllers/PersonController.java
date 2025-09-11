b912e0543210817f2bf32875594c82909bb66615
andreas <andreasnunesmachado2016@gmail.com>
2025-09-11 01:04:31 -0300

    cors implemented

--- restSpringboot/src/main/java/br/com/andreas/controllers/PersonController.java
+++ restSpringboot/src/main/java/br/com/andreas/controllers/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -25,12 +26,14 @@ public class PersonController implements PersonControllerDocs {
         return service.findAll();
     }
 
+//    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     @Override
     public PersonDTO findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
 
+//    @CrossOrigin(origins = {"http://localhost:8080"})
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     @Override
     public PersonDTO create(@RequestBody PersonDTO person){
